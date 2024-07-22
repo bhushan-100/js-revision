6326be98552aaf11718308d831078ccda8f527a0
bhushan-100 <bhushansjadhav007@gmail.com>
2024-07-22 20:13:37 +0530

    datatypes_summary and types like primitive and non primitive

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,16 +3,38 @@
 
 let name="bhushan"
 let age="21"
-let isLogggedIn= fals
+let isLogggedIn= false
 let state;
 console.log(typeof null);
 console.log(typeof undefined);
 // number ->2 to power 53
 //big int
+const bigNumber = 4759947902849n
+console.log(typeof(bigNumber))
+
+console.log
 // string=> ""
 //boolean => true/false
 //null => standalone value MATLAB MAI BOL RHA BHAI YE KHALI HAI NOT UNDEFINED 
 //UNDEFINED
 //symbol =>unique
 
-//object
+// Symbol
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id === anotherid)
+
+// non primitive Array,Objects,Functions
+const heroes =["ironman","captain america"];
+
+let myObj ={
+    name:"bhushan",
+    age:21,
+}
+const myFunction= function(){
+    console.log("hello world");
+
+}
+console.log(typeof(myFunction))//but this is object function it just returns function
+console.log(typeof(heroes))
+console.log(typeof(myObj))
---
+++ 01_basics/05_datatypes_interview.md
@@ -0,0 +1,14 @@
+
+
+# 1.primitive (call by value)
+## 7 types
+### 1.String 2.Number3.Boolean 4.null 5.undefined 6.Symbol 7.BigInt
+
+
+
+# 2.Reference or Non primitive (call by reference)
+### Array,Objects,Functions
+
+### Javascript is dynamically typed language datat types of variables are determined by value they hold at runtime 
+
+
